0611198a1285e0dadf33ffa28ebb1fe3cb6d88b8
Ali Konar <ali.knr@hotmail.com>
2021-02-16 15:13:04 +0300

    .

---
+++ Business/Abstract/Melek.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    class Melek
+    {
+        // SENİ SEVİYORUM SEVGİLİM
+        // İYİ Kİ VARSIN 
+        // İYİ Kİ HAYTIMDASIN
+    }
+}
